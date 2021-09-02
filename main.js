5a4eaeb9e53f2303b85f237064d51212f8d270bd
AkkuAkhil <akhildev.adj@gmail.com>
2021-09-02 15:11:32 +0530

    Game 3 Images Added

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const sharp = require("sharp");
 const fs = require("fs");
-const basePath = "./original/";
-const game = 1;
+const basePath = "./original";
+const game = 3;
 
-fs.readdir(basePath, async function (err, files) {
+fs.readdir(`${basePath + game}/`, async function (err, files) {
   if (err) {
     return console.log("Unable to scan directory: " + err);
   }
 
   await Promise.all(
     files.map(async (fileName, index) => {
-      await sharp(basePath + fileName)
+      await sharp(`${basePath + game}/` + fileName)
         .resize(400, 500)
         .jpeg({ quality: 100 })
         .toFile(`./images/game${game}/` + (index + 1) + ".jpeg");
