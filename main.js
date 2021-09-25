b58cf41b532c2fa982b5050f0cce237dacc960ac
AkkuAkhil <akhildev.adj@gmail.com>
2021-09-25 12:41:16 +0530

    Updated MAD

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const sharp = require("sharp");
 const fs = require("fs");
 const basePath = "./original";
-const game = 6;
+const game = 1;
 
 fs.readdir(`${basePath + game}/`, async function (err, files) {
   if (err) {
@@ -12,7 +12,7 @@ fs.readdir(`${basePath + game}/`, async function (err, files) {
     files.map(async (fileName, index) => {
       await sharp(`${basePath + game}/` + fileName)
         .resize(400, 500)
-        .jpeg({ quality: 100 })
+        .jpeg({ quality: 85 })
         .toFile(`./images/game${game}/` + (index + 1) + ".jpeg");
     })
   );
